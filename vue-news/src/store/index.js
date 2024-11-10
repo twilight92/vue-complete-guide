32adfe30a8d94e61f26d7ad438c24a059eb8fcd8
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-10 18:42:17 +0900

    스토어 속성 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchNewsList, fetchJobsList, fetchAskList } from "../api/index.js";
+import mutations from "./mutations.js";
+import actions from "./actions.js";
 
 Vue.use(Vuex);
 
@@ -15,46 +16,8 @@ const store = new Vuex.Store({
       return state.ask;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS({ commit }) {
-      fetchNewsList()
-        .then(({ data }) => {
-          commit("SET_NEWS", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit("SET_JOBS", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit("SET_ASK", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
+  mutations,
+  actions,
 });
 
 export default store;

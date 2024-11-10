294dfc859bcc188b9a8f49d4792aa8d5af9ba5b8
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-10 18:38:28 +0900

    map 헬퍼 함수를 이용한 AskView 적용

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
     ask
-    <div v-for="item in this.$store.state.ask" :key="item">
+    <div v-for="item in fetchedAsk" :key="item">
       {{ item.title }}
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
+  computed: {
+    ...mapGetters({
+      fetchedAsk: "fetchedAsk",
+    }),
+    // #2
+    // ...mapState({
+    //     fetchedAsk: state => state.ask;
+    // })
+
+    // #1
+    // ask() {
+    //     return this.$store.state.ask;
+    // }
+  },
   created() {
     this.$store.dispatch("FETCH_ASK");
   },

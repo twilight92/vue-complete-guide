04f7463b21f5df4e963cbe3b9c98df4e3fbe57f3
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-10 18:17:51 +0900

    NewsView에 actions와 mutations 적용

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,11 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { fetchNewsList } from "../api/index.js";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     news: [],
+  },
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    },
+  },
+  actions: {
+    FETCH_NEWS(context) {
+      fetchNewsList()
+        .then((response) => {
+          context.commit("SET_NEWS", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 });
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news" :key="user">
+      {{ user.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from "../api/index.js";
-
 export default {
-  data() {
-    return {
-      users: [],
-    };
-  },
   created() {
-    fetchNewsList()
-      .then((response) => (this.users = response.data))
-      .catch((error) => {
-        console.log(error);
-      });
+    this.$store.dispatch("FETCH_NEWS");
   },
 };
 </script>
